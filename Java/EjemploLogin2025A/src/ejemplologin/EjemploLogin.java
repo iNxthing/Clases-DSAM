f0d0f30e869c7154aa46127bd5e8f28bba69506a
Darwin Garcia M. <darwingarciam1553@gmail.com>
2026-09-25 10:53:13 -0500

    Ejemplos para el proyecto

---
+++ Java/EjemploLogin2025A/src/ejemplologin/EjemploLogin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplologin;
+
+/**
+ *
+ * @author USER
+ */
+public class EjemploLogin {
+
+
+    
+}
---
+++ Java/EjemploLogin2025A/src/ejemplologin/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplologin;
+
+/**
+ *
+ * @author USER
+ */
+public class Usuario {
+    
+    private int id;
+    private String user;
+    private String pass;
+    private int idProfile;
+
+    public Usuario(int id, String user, String pass, int idProfile) {
+        this.id = id;
+        this.user = user;
+        this.pass = pass;
+        this.idProfile = idProfile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public int getIdProfile() {
+        return idProfile;
+    }
+    
+    
+    
+    
+}
